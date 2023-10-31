54c37a4e596a3f158d06b7d40ece0aec7f09aa51
Luis Mendoza <luism110803@gmail.com>
2023-10-30 20:10:14 -0500

    actulizacion de interfaz HMI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow,  QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 from PyQt5.uic import loadUi
+from pyqtgraph.opengl import GLLinePlotItem
 import pyqtgraph.opengl as gl
 from PyQt5.QtGui import QColor
 # Taller # 1
@@ -11,15 +12,37 @@ class MainApp(QMainWindow):
     def __init__(self):
         super(MainApp, self).__init__()
         loadUi("HMI_interfaz.ui", self)
+        global distancia
+        distancia = 35
         self.setWindowTitle("Gráfica 3D con PyQt5 y Matplotlib")
         # Configura la gráfica 3D
         self.view3D = gl.GLViewWidget()
-        self.view3D.setCameraPosition(distance=30, elevation=30, azimuth=45)
-        self.view3D.setBackgroundColor(QColor(250,250,228))
+        self.view3D.setCameraPosition(distance=distancia, elevation=30, azimuth=225)
+        # self.view3D.setBackgroundColor(QColor(179,226,255))
+        self.view3D.setBackgroundColor(QColor(0,0,0))
         self.grid0 = gl.GLGridItem()
-        self.grid0.setColor(QColor(0,0,0))
-        self.grid0.setSize(15,25,1)
+        self.grid0.setColor(QColor(255,255,255))
+        self.grid0.setSize(20,28,1)
         self.view3D.addItem(self.grid0)
+        # Crear ejes X, Y, Z personalizados
+        axis_width = 5  # Puedes ajustar este valor según tu preferencia
+
+        # Eje X (rojo)
+        x_axis_points = np.array([[-10, 14, 0], [-9, 14, 0]])
+        x_axis = GLLinePlotItem(pos=x_axis_points, color=(1, 0, 0, 1), width=axis_width)
+
+        # Eje Y (verde)
+        y_axis_points = np.array([[-10, 14, 0], [-10, 13, 0]])
+        y_axis = GLLinePlotItem(pos=y_axis_points, color=(0, 1, 0, 1), width=axis_width)
+
+        # Eje Z (azul)
+        z_axis_points = np.array([[-10, 14, 0], [-10, 14, 1]])
+        z_axis = GLLinePlotItem(pos=z_axis_points, color=(0, 0, 1, 1), width=axis_width)
+
+        # Agregar ejes a la vista 3D
+        self.view3D.addItem(x_axis)
+        self.view3D.addItem(y_axis)
+        self.view3D.addItem(z_axis)
         self.plotWidget.addWidget(self.view3D)
 
         self.iso_button.clicked.connect(self.set_isometric_view)
@@ -31,62 +54,28 @@ class MainApp(QMainWindow):
 
         self.top_button.clicked.connect(self.set_top_view)
         # Agrega una esfera de ejemplo
-        # self.plot_example_sphere()
+        self.plot_example_cone()
 
-    def plot_example_sphere(self):
-        # Crear una esfera de ejemplo
-        sphere = gl.MeshData.sphere(rows=10, cols=20, radius=1.0)
-        sphere_mesh = gl.GLMeshItem(meshdata=sphere, color=(1.0, 0.0, 0.0, 1.0))
-        sphere_mesh.translate(0, 0, 0)  # Posición de la esfera en coordenadas (x, y, z)
-                # Agregar la esfera a la vista
-        self.view3D.addItem(sphere_mesh)
-
+    def plot_example_cone(self):
+        # Crear un cono de ejemplo
+        cone = gl.MeshData.cylinder(rows=10, cols=20, radius=[0.0, 0.5], length=2.0)
+        cone_mesh = gl.GLMeshItem(meshdata=cone, color=(1.0, 0.0, 0.0, 1.0))
+        cone_mesh.translate(-10, 14, 0)  # Posición del cono en coordenadas (x, y, z)
         
+        # Agregar el cono a la vista 3D
+        self.view3D.addItem(cone_mesh)
 
     def set_isometric_view(self):
-        self.view3D.setCameraPosition(distance=30, elevation=30, azimuth=45)
+        self.view3D.setCameraPosition(pos=(0, 0, 0), distance=distancia, elevation=30, azimuth=225)
 
     def set_front_view(self):
-        self.view3D.setCameraPosition(distance=30, elevation=0, azimuth=0)
+        self.view3D.setCameraPosition(distance=distancia, elevation=0, azimuth=90+180)
 
     def set_side_view(self):
-        self.view3D.setCameraPosition(distance=30, elevation=0, azimuth=90)
+        self.view3D.setCameraPosition(distance=distancia, elevation=0, azimuth=180)
 
     def set_top_view(self):
-        self.view3D.setCameraPosition(distance=25, elevation=90, azimuth=90)
-
-    # def parsOBJ(self, path):
-    #     print('loading', path)
-
-    #     vertices = []
-    #     texture_coordinates = []
-    #     faces = []
-    #     try: 
-    #         f = open(path, 'r')
-    #         for line in f:
-    #             if line[0] != '#':
-    #                 line_parts = line.replace('\n', '').split(' ')
-    #                 if line_parts[0] == 'v':
-    #                     vertex = [float(line_parts[1]), float(line_parts[2]), float(line_parts[3])]
-    #                     vertices.append(vertex)
-    #                 elif line_parts[0] == 'vt':
-    #                     tex_coord = [float(line_parts[1]), float(line_parts[2])]
-    #                     texture_coordinates.append(tex_coord)
-    #                 elif line_parts[0] == 'f' and len(line_parts) == 5:
-    #                     face = [int(line_parts[1].split('/')[0])-1, int(line_parts[2].split('/')[0])-1, int(line_parts[3].split('/')[0])-1, int(line_parts[4].split('/')[0])-1]
-    #                     faces.append(face)
-    #         f.close()
-
-    #         gl_vertices = []
-    #         for face in faces:
-    #             gl_vertices.append(vertices[face[0]])
-    #             gl_vertices.append(vertices[face[1]])
-    #             gl_vertices.append(vertices[face[2]])
-    #             gl_vertices.append(vertices[face[3]])
-    #     except Exception as e: 
-    #         print("error: " +str(e))
-    #     return np.asarray(gl_vertices)
-
+        self.view3D.setCameraPosition(distance=distancia, elevation=90, azimuth=-90)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
